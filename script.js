0f61815b5e99950ef7cc9c501ef3ab0517e1cede
Rohit Kumar Ranjan <119317461+Rohitkrranjan@users.noreply.github.com>
2023-07-19 11:49:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("Welcome to my portfolio");
+function WhatAppFN(){
+    // Create an anchor element
+    let link = document.createElement("a");
+       // Set the href attribute to the Google URL
+    link.href = "https://wa.me/916205041054"
+      // Open the link in a new tab
+    link.target = "_blank";
+     // Click the link programmatically
+    link.click()
+}
+  
